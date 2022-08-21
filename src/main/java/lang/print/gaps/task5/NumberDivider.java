61964d0707022e06ead830be482836afd3dd5687
patifnati <rzezniczekpatrycja@gmail.com>
2022-08-21 08:57:52 +0200

    almost done

--- src/main/java/lang/print/gaps/task5/NumberDivider.java
+++ src/main/java/lang/print/gaps/task5/NumberDivider.java
@@ -2,5 +2,7 @@ package lang.print.gaps.task5;
 
 public class NumberDivider {
     void divide(int toBeDivided) {
+        double numberDivider = 5.0;
+        System.out.println(toBeDivided / numberDivider);
     }
 }
--- src/main/java/lang/print/gaps/task5/TimeConvertor.java
+++ src/main/java/lang/print/gaps/task5/TimeConvertor.java
@@ -1,6 +1,12 @@
 package lang.print.gaps.task5;
 
+import java.sql.SQLOutput;
+
 public class TimeConvertor {
     public void convert(float minutes) {
+
+        float seconds = 60.0F;
+        float formula = minutes * seconds;
+        System.out.println(formula);
     }
 }
